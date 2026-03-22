1660b381324c635de7d313e1793e887aecf62410
Ebuka Gideon Igbo <igbogideonebuka@gmail.com>
2026-03-22 07:21:58 -0700

    Added logout route to authRouter

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,5 +1,5 @@
 const  express = require('express')
-const { signIn, resetPassword, resetRequest, validationPasswordOtp } = require('../controllers/authController')
+const { signIn, resetPassword, resetRequest, validationPasswordOtp, signOut } = require('../controllers/authController')
 
 
 const authRouter = express.Router()
@@ -8,6 +8,9 @@ authRouter
 
 //login a user
   .post('/user/login', signIn)
+
+//logout a user
+  .post('/user/logout', signOut)
 
 //reset a user's password
   .post('/password/resetReq', resetRequest)
